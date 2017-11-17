459fceeccd01e61d6025b8ae7fb7a463fb0d93ec
dfghjm <popov@ucu.edu.ua>
2017-11-17 10:46:36 +0200

    ~

--- src/test/java/com/CharacterFactoryTest.java
+++ src/test/java/com/CharacterFactoryTest.java
@@ -9,16 +9,14 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class CharacterFactoryTest {
-    CharacterFactory characterFactory = new CharacterFactory();
-    Character character = characterFactory.createCharacter();
     @Test
     public void createCharacter() throws Exception {
         CharacterFactory characterFactory = new CharacterFactory();
         List<Class> arr = new ArrayList<>(Arrays.asList(Hobbit.class, Knight.class, King.class, Elf.class));
-        for (int i = 0; i < 25; i++) {
-            Character res = characterFactory.createCharacter();
-            if (res != null){
-                assertEquals(true,arr.contains(res.getClass()));}}
+        for (int i = 0; i < 15; i++) {
+            Character character = characterFactory.createCharacter();
+            if (character != null){
+                assertTrue(arr.contains(character.getClass()));}}
     }
 
 }
