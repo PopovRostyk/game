eea59dd8e0b175014ee5362617c35eb51a291917
dfghjm <popov@ucu.edu.ua>
2017-11-17 10:39:47 +0200

    Change tets

--- src/test/java/com/CharacterFactoryTest.java
+++ src/test/java/com/CharacterFactoryTest.java
@@ -2,6 +2,10 @@ package com;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CharacterFactoryTest {
@@ -9,7 +13,12 @@ public class CharacterFactoryTest {
     Character character = characterFactory.createCharacter();
     @Test
     public void createCharacter() throws Exception {
-        assertEquals(new Elf().getClass().getSuperclass().getSimpleName(), character.getClass().getSuperclass().getSimpleName());
+        CharacterFactory characterFactory = new CharacterFactory();
+        List<Class> arr = new ArrayList<>(Arrays.asList(Hobbit.class, Knight.class, King.class, Elf.class));
+        for (int i = 0; i < 25; i++) {
+            Character res = characterFactory.createCharacter();
+            if (res != null){
+                assertEquals(true,arr.contains(res.getClass()));}}
     }
 
 }
